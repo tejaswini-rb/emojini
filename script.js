f5b4ab179a705d3dae39848823ed4d8fdea04b14
tejaswini-rb <114116509+tejaswini-rb@users.noreply.github.com>
2024-09-28 11:28:11 -0400

    edited changes

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-import images from "./imag.js"
-
-const memesOrGifs = images;
-let currentCatIndex = 0;
 let buttonClicked = false;
 
-function toggleCatGif() {
-    currentCatIndex = (currentCatIndex + 1) % memesOrGifs.length;
-    const gifContainer = document.getElementById('gif-container');
-    const catGif = document.getElementById('meme-gif');
-    catGif.src = memesOrGifs[currentCatIndex];
-    gifContainer.classList.remove('hidden');
+function addEmoji() {
 
-
-    
-
-    // hide start screen once event button is clicked
-    const start = document.querySelector('.start');
-    if (!buttonClicked) {
-        start.classList.add('hidden');
-        const buttonIcon = document.querySelector('.button-icon');
-        buttonIcon.textContent = 'click for more goofy meme :3';
-        buttonClicked = true;
-    }
 }
 
 // event listener for the button
