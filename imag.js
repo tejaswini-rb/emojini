e7abdb935110e43346656993dcfd8cf46c4d857f
tejaswini-rb <114116509+tejaswini-rb@users.noreply.github.com>
2024-09-28 11:28:24 -0400

    Delete imag.js

--- imag.js
+++
@@ -1,5 +0,0 @@
-let images;
-
-export default images = ["./images/image0.jpg", "./images/image1.jpg", "images/image2.jpg", "images/buzz_mewing.gif", "images/image3.jpg", "images/image4.png","images/image5.png", 
-    "images/image6.png", "images/image7.png", "images/countries.png", "images/mewing.png", "images/nnn.jpg", "images/image8.png", "images/mewing2.gif", "images/image9.png", "images/griddy.gif", "images/image10.png", "images/image12.jpg", "images/image13.png", 
-    "images/image11.png", "images/cooking.jpeg", "images/image15.png", "images/image14.png", "images/arctic-monkets.jpg", "images/game.png"]
